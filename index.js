7660252149b8b3c216c1902f30bc0b126aac0efb
Adam Hammer <adam.hammer@abbyy.com>
2021-06-02 22:59:05 +0200

    lots

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import crypto from 'crypto';
+
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -14,33 +16,92 @@ mongoose.connect(
     )}@shortr.xwnwx.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true }
 );
+
 const Link = mongoose.model('Link', {
     url: String,
     name: String,
     hitCount: Number,
 });
 
+const generareName = async (length = 2) => {
+    const name = crypto.randomBytes(length).toString('hex');
+    if (await Link.exists({ name })) {
+        return generareName();
+    }
+
+    return name;
+};
+
+const validateUrl = (string) => {
+    let url;
+
+    try {
+        url = new URL(string);
+    } catch {
+        return false;
+    }
+
+    return url.protocol === 'http:' || url.protocol === 'https:';
+};
+
+const validateName = (string) => /^[A-z0-9-._]+$/.test(string);
+
+const validate = async (req, res) => {
+    if (!req.body.url) {
+        res.status(400);
+        res.json({
+            error: 'url_required',
+            field: 'url',
+        });
+    }
+
+    if (!validateUrl(req.body.url)) {
+        res.status(400);
+        res.json({
+            error: 'url_invalid',
+            field: 'url',
+        });
+    }
+
+    if (req.body.name && (await Link.exists({ name: req.body.name }))) {
+        res.status(400);
+        res.json({
+            error: 'name_taken',
+            field: 'name',
+        });
+    }
+
+    if (req.body.name && !validateName(req.body.name)) {
+        res.status(400);
+        res.json({
+            error: 'name_invalid',
+            field: 'name',
+        });
+    }
+};
+
 const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/links', (req, res) => {
-    res.sendFile(path.join(__dirname+'/links.html'))
-})
+app.get('/links', (_req, res) => {
+    res.sendFile(path.join(__dirname + '/links.html'));
+});
 
 app.post('/api/url', async (req, res) => {
-    if (await Link.exists({ name: req.body.name })) {
-        res.status(400);
-        res.json({
-            error: 'name already taken',
-        });
-    }
+    await validate(req, res);
 
-    await Link.create({ name: req.body.name, url: req.body.url, hitCount: 0 });
+    const name = req.body.name || (await generareName());
+
+    await Link.create({ name, url: req.body.url, hitCount: 0 });
+    res.json({
+        name,
+        url: req.body.url,
+    });
     res.end();
 });
 
-app.get('/api/url', async (req, res) =>
+app.get('/api/url', async (_req, res) =>
     res.json(
         (await Link.find()).map((link) => ({
             url: link.url,
@@ -50,11 +111,31 @@ app.get('/api/url', async (req, res) =>
     )
 );
 
+app.get('/api/url/:name', async (req, res) => {
+    if (!req.params.name) {
+        res.status(400);
+        res.json({
+            error: 'name_required',
+            field: 'name',
+        });
+    }
+
+    const link = await Link.findOne({ name: req.params.name });
+
+    if (!link) {
+        res.status(404);
+        res.end();
+    }
+
+    res.json({ link });
+    res.end();
+});
+
 app.get('/:name', async (req, res) => {
     const link = await Link.findOne({ name: req.params.name });
     if (!link) {
         res.status(404);
-        res.sendFile(path.join(__dirname+'/404.html'))
+        res.sendFile(path.join(__dirname + '/404.html'));
     }
     await link.update({ hitCount: ++link.hitCount });
     res.redirect(link.url);
---
+++ public/index.js
@@ -0,0 +1,175 @@
+const createButton = document.querySelector('.create-container button');
+const checkButton = document.querySelector('.check-container button');
+const button = document.querySelector('.create-container button');
+const urlInput = document.querySelector('input[name="url"]');
+const nameInput = document.querySelector('.create-container input[name="name"]');
+const checkNameInput = document.querySelector('.check-container input[name="name"]');
+const createErrorMessage = document.querySelector('.create-container .error');
+const checkErrorMessage = document.querySelector('.check-container .error');
+const createSuccess = document.querySelector('.create-container .success');
+const checkSuccess = document.querySelector('.check-container .success');
+const copy = document.querySelector('.copy');
+const copied = document.querySelector('.copied');
+const urlError = document.querySelector('.url-error');
+const nameError = document.querySelector('.name-error');
+const createNameError = document.querySelector('.create-container .name-error');
+const checkNameError = document.querySelector('.check-container .name-error');
+const navItems = document.querySelectorAll('nav ul li');
+const containers = document.querySelectorAll('main > *');
+
+const errors = {
+    name_taken: 'Name is already taken!',
+    name_invalid: 'Invalid name! (A-z0-9-_.)',
+    url_invalid: 'Invalid URL!',
+    url_required: 'URL is required!',
+    unknown: 'Something bad happened. Please try again later!'
+};
+
+const copyToClipboard = (string) => {
+    const element = document.createElement('textarea');
+    element.value = string;
+    document.body.appendChild(element);
+    element.select();
+    document.execCommand('copy');
+    document.body.removeChild(element);
+};
+
+const validateUrl = (string) => {
+    let url;
+  
+    try {
+      url = new URL(string);
+    } catch {
+      return false;  
+    }
+  
+    return url.protocol === "http:" || url.protocol === "https:";
+}
+
+const validateName = (string) => /^[A-z0-9-._]+$/.test(string);
+
+urlInput.addEventListener('input', () => urlError.classList.remove('show'));
+
+nameInput.addEventListener('input', () => nameError.classList.remove('show'));
+
+createButton.addEventListener('click', async () => {
+    createErrorMessage.classList.add('d-none');
+    createSuccess.classList.add('d-none');
+    urlError.classList.remove('show');
+    nameError.classList.remove('show');
+
+    if (!urlInput.value) {
+        urlError.textContent = errors.url_required;
+        urlError.classList.add('show');
+        return;
+    }
+
+    if (!validateUrl(urlInput.value)) {
+        urlError.textContent = errors.url_invalid;
+        urlError.classList.add('show');
+        return;
+    }
+
+    if (nameInput.value && !validateName(nameInput.value)) {
+        nameError.textContent = errors.name_invalid;
+        nameError.classList.add('show');
+        return;
+    }
+
+    const response = await fetch('/api/url', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            url: urlInput.value,
+            name: nameInput.value,
+        }),
+    });
+
+    if (!response.ok) {
+        const { error, field } = await response.json();
+
+        switch (field) {
+            case 'name': {
+                nameError.textContent = errors[error];
+                nameError.classList.add('show');
+                break;
+            }
+            case 'url': {
+                urlError.textContent = errors[error];
+                urlError.classList.add('show');
+                break;
+            }
+            default: {
+                createErrorMessage.textContent = errors.unknown;
+                createErrorMessage.classList.remove('d-none');
+                break;
+            }
+        }
+
+        nameError.textContent = errors[error];
+        nameError.classList.add('show');
+        return;
+    }
+    const { name } = await response.json();
+
+    createSuccess.classList.remove('d-none');
+    copy.classList.remove('d-none');
+    copied.classList.add('d-none');
+    createSuccess.setAttribute('data-url', new URL(name, window.location.origin));
+});
+
+checkButton.addEventListener('click', async () => {
+    checkNameError.classList.remove('show');
+    checkErrorMessage.classList.add('d-none');
+    checkSuccess.classList.add('d-none');
+
+    if (!validateName(checkNameInput.value)) {
+        checkNameError.textContent = errors.name_invalid;
+        checkNameError.classList.add('show');
+        return;
+    }
+
+    const response = await fetch(`/api/url/${checkNameInput.value}`);
+
+    if (!response.ok) {
+        if (response.status === 404) {
+            checkErrorMessage.classList.remove('d-none');
+            return;
+        }
+        const { error } = await response.json();
+        checkNameError.textContent = errors[error];
+        checkNameError.classList.add('show');
+    }
+
+    const { link } = await response.json();
+
+    checkSuccess.classList.remove('d-none');
+    checkSuccess.querySelector('span').textContent = link.hitCount;
+    checkSuccess.querySelector('a').href = link.url;
+    checkSuccess.querySelector('a').textContent = link.url;
+
+    console.log(response);
+});
+
+createSuccess.addEventListener('click', () => {
+    copyToClipboard(createSuccess.getAttribute('data-url'));
+    copy.classList.add('d-none');
+    copied.classList.remove('d-none');
+});
+
+navItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        containers.forEach((container) => {
+            container.classList.add('d-none');
+        });
+
+        navItems.forEach((item) => {
+            item.classList.remove('active');
+        });
+
+        document.querySelector(`.${item.getAttribute('data-container')}-container`).classList.remove('d-none');
+        item.classList.add('active');
+    })
+});
