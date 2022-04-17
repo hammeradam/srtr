f3583299e3e4b12a6b23269998b99b3bfebd97d9
Adam Hammer <adam.hammer@abbyy.com>
2022-04-18 01:04:14 +0200

    dsfsdf

--- replaceBaseUrl.js
+++ replaceBaseUrl.js
@@ -6,7 +6,11 @@ fs.readFile(
     function (err, data) {
         const result = data
             .replace(/http:\/\/localhost:3000/g, 'https://srtr.herokuapp.com')
-            .replace(/9fd26d2f35d5520e4f3a/, process.env.GITHUB_CLIENT_ID);
+            .replace(/9fd26d2f35d5520e4f3a/, process.env.GITHUB_CLIENT_ID)
+            .replace(
+                /697090043703-5v8qd9p8efre0pdfo6s2c2ci5k9pld16.apps.googleusercontent.com/,
+                process.env.GOOGLE_CLIENT_ID
+            );
 
         fs.writeFile(
             './public/js/utils/authentication.js',
