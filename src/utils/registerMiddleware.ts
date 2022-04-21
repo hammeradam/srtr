45f672b8d0265ae26437d943012ed61cc01ead09
Adam Hammer <adam.hammer@abbyy.com>
2022-04-21 23:35:08 +0200

    session to mongodb in production

--- src/utils/registerMiddleware.ts
+++ src/utils/registerMiddleware.ts
@@ -1,19 +1,30 @@
 import { Express, json, urlencoded, static as serveStatic } from 'express';
-import session from 'express-session';
+import session, { SessionOptions } from 'express-session';
 import cookieParser from 'cookie-parser';
+import MongoStore from 'connect-mongo';
 import { loggerMiddleware } from 'middleware';
 
 export const registerMiddleware = (app: Express) => {
+    const sessionConfig: SessionOptions = {
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+    };
+
+    if (process.env.NODE_ENV === 'production') {
+        sessionConfig.store = MongoStore.create({
+            mongoUrl: `mongodb+srv://${
+                process.env.DB_USER
+            }:${encodeURIComponent(process.env.DB_PASSWORD)}@${
+                process.env.DB_HOST
+            }/${process.env.DB_NAME}`,
+        });
+    }
+
     app.use(json());
     app.use(urlencoded({ extended: true }));
     app.use(serveStatic('public'));
     app.use(cookieParser());
     app.use(loggerMiddleware);
-    app.use(
-        session({
-            secret: process.env.SESSION_SECRET,
-            resave: false,
-            saveUninitialized: true,
-        })
-    );
+    app.use(session(sessionConfig));
 };
