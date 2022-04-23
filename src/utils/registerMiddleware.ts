17344e5e9320a930cf63ac1c4ce9ff795957c78d
Adam Hammer <adam.hammer@abbyy.com>
2022-04-23 13:48:32 +0200

    compression

--- src/utils/registerMiddleware.ts
+++ src/utils/registerMiddleware.ts
@@ -1,7 +1,7 @@
 import { Express, json, urlencoded, static as serveStatic } from 'express';
 import session, { SessionOptions } from 'express-session';
 import cookieParser from 'cookie-parser';
-// import MongoStore from 'connect-mongo';
+import compression from 'compression';
 import { loggerMiddleware } from 'middleware';
 
 export const registerMiddleware = (app: Express) => {
@@ -11,16 +11,7 @@ export const registerMiddleware = (app: Express) => {
         saveUninitialized: true,
     };
 
-    // if (process.env.NODE_ENV === 'production') {
-    //     sessionConfig.store = MongoStore.create({
-    //         mongoUrl: `mongodb+srv://${
-    //             process.env.DB_USER
-    //         }:${encodeURIComponent(process.env.DB_PASSWORD)}@${
-    //             process.env.DB_HOST
-    //         }/${process.env.DB_NAME}`,
-    //     });
-    // }
-
+    app.use(compression());
     app.use(json());
     app.use(urlencoded({ extended: true }));
     app.use(serveStatic('public'));
