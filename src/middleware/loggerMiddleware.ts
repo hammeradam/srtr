374784812be1a07c78cece9afae5f01aaf30d78d
Adam Hammer <adam.hammer@abbyy.com>
2022-04-25 16:36:25 +0200

    detaild log

--- src/middleware/loggerMiddleware.ts
+++ src/middleware/loggerMiddleware.ts
@@ -1,9 +1,16 @@
 import { RequestHandler } from 'express';
 import { cyan, green, magenta } from 'utils/color';
-
+import { UAParser } from 'ua-parser-js';
 export const loggerMiddleware: RequestHandler = (req, _, next) => {
+    const ua = new UAParser(req.get('User-Agent')).getResult();
+
+    console.log(ua);
+
     console.log(
-        `${magenta('REQUEST')} ${cyan(req.method.padEnd(4))} ${green(req.path)}`
+        `${magenta('REQUEST')} ${cyan(req.method.padEnd(4))} ${green(
+            req.path
+        )} ${cyan(req.ip)}`
     );
+
     next();
 };
