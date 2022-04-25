a9ba2f34a418540d82c0dadd341a5532f01ff295
Adam Hammer <adam.hammer@abbyy.com>
2022-04-25 18:01:30 +0200

    log x-forwarded-fot

--- src/middleware/loggerMiddleware.ts
+++ src/middleware/loggerMiddleware.ts
@@ -5,6 +5,7 @@ export const loggerMiddleware: RequestHandler = (req, _, next) => {
     const ua = new UAParser(req.get('User-Agent')).getResult();
 
     console.log(ua);
+    console.log(req.headers['x-forwarded-for']);
 
     console.log(
         `${magenta('REQUEST')} ${cyan(req.method.padEnd(4))} ${green(
