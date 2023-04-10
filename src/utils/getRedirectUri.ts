31e39afcdf5ba6bb64a929572294a3b78440176e
Adam Hammer <adam.hammer@abbyy.com>
2023-04-10 23:46:07 +0200

    use url constructor in getRedirectUri

--- src/utils/getRedirectUri.ts
+++ src/utils/getRedirectUri.ts
@@ -1,2 +1,4 @@
-export const getRedirectUri = (provider: string) =>
-    `${process.env.BASE_URL}/api/auth/callback/${provider}`;
+export const getRedirectUri = (provider: string) => {
+    const url = new URL(`/api/auth/callback/${provider}`, process.env.BASE_URL);
+    return url.href;
+};
